6f99e803a8c15dae9683a6234f5cbb459bf936e9
Naufal Achmad Zamroni <naufalachmad60@gmail.com>
2024-01-15 13:20:24 +0700

    ini adalah commit yang pertama

---
+++ src/Route/bangunDatar.ts
@@ -0,0 +1,20 @@
+import  Express  from "express";
+import { kelilingLingkaran, kelilingPersegi, kelilingPersegiPanjang, luasLingkaran, luasPersegi, luasPersegiPanjang, luasSegitiga} from "../Controller/bangunDatar";
+const app =  Express()
+
+
+/**allow read a body */
+app.use(Express.json())
+
+/**fungsi use()digunakan untuk menerapkan sebuah fungsi pada object express fungsi tersebut akan otomatis 
+ * dijalankan
+ */
+
+app.post(`/lingkaran/luas`,luasLingkaran)
+app.post(`/lingkaran/keliling`,kelilingLingkaran)
+app.post(`/persegi/luas`,luasPersegi)
+app.post(`/persegi/keliling`,kelilingPersegi)
+app.post(`PersegiPanjang/luas`,luasPersegiPanjang)
+app.post(`PersegiPanjang/keliling`,kelilingPersegiPanjang)
+app.post(`Segitiga/luas`,luasSegitiga)
+export default app
---
+++ src/index.ts
@@ -0,0 +1,87 @@
+/**ini adalah file utama */
+/** dimana ada proses menjalankan server backend
+ */
+
+/**memanggil library express */
+import express, { Request, Response, request } from "express"
+import { validateCube } from "./middleware/validateCube"
+import routeBangunDatar from "./Route/bangunDatar"
+import routebangunRuang from "./Route/bangunRuang"
+
+/**buat wadah untuk inisiasi express */
+const app=express()
+
+/**mendefinisikan port berjalannya server */
+const PORT=8000
+/**allow to read Json as request */
+app.use(express.json())
+
+/**proses pertama untuk handle request */
+app.get(`/serena`,(request: Request,response: Response)=>{
+    /**
+     * ini adalah proses handle request dengan 
+     * url/address: https//localhost:8000/serena
+     * method get
+     */
+    /**buatlah request konversi suhu celcius kefahrenheit,kelvin,reamur
+     * menggunakan request parameter
+     * exp:http://localhost:8000/celcius/80
+     * 80 nilai celcius
+     * reamur:?,fahrenhait,kelvin
+     */
+    return response.status(200).json({
+        message:`Hello Serena Anaknya Bu Siana`
+    })
+})
+
+/**read a query request */
+app.get(`/moklet`,(request: Request,response: Response)=>{
+    /** Asumsikan data yang dikirim adalah nama dan umur
+     * 
+    */
+   let nama:any=request.query.nama?.toString()
+   let umur:any=request.query.umur?.toString()
+
+   let message : string = `My name is ${nama} and i'm ${umur} years old`
+    return response.status(200).json(message)
+})
+
+/**read data request from parameter */
+app.get(`/telkom/:nama/:gender`,(request:Request,response:Response)=>{
+    let nama:string=request.params.nama
+    let gender:string=request.params.gender
+    let message:string=`My name is ${nama} and i'm ${gender}`
+    return response.status(200).json(message)
+})
+
+/**read a reuest from body */
+app.post(`/moklet`,(request:Request,response:Response)=>{
+    /**asumsikan data yang dikirimkan pannjang dan lebar */
+    let panjang:number=request.body.panjang
+    let lebar:number=request.body.lebar
+
+    let luasPersegiPanjang:number=panjang*lebar
+    let message=`luas persegi panjang adalah ${luasPersegiPanjang}`
+    return response.status(200).json(message)
+})
+
+/**create request for count volume of long cube */
+app.post(`/balok`,validateCube ,(request:Request,response:Response)=>{
+    /**read panjjang,lebar,tinggi */
+    let panjang:number=Number(request.body.panjang)
+    let lebar:number=Number(request.body.lebar)
+    let tinggi:number=Number(request.body.tinggi)
+
+    let volume:number=panjang*lebar*tinggi
+    return response.status(200).json({
+        panjang,lebar,tinggi,volume
+    })
+})
+/**register route of bangun datar */
+app.use(routeBangunDatar)
+app.use(routebangunRuang)
+
+/**run server */
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
